4f53cd8febbb495353402e61d19ef5f45fd9fb3d
KYPremco <KYPremco@gmail.com>
2024-07-05 10:00:43 +0200

    initial

---
+++ TimberPrint/Blueprint.cs
@@ -0,0 +1,11 @@
+namespace TimberPrint;
+
+public class Blueprint
+{
+    public BlueprintItem[] BlueprintItems { get; }
+
+    public Blueprint(BlueprintItem[] blueprintItems)
+    {
+        BlueprintItems = blueprintItems;
+    }
+}
